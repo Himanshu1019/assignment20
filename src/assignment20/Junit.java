70489aee389632d4686412453a4dc0980f044ea7
classroom <himanshukanoje23@gmail.com>
2022-04-18 08:54:05 +0530

    my commit

--- src/assignment20/Junit.java
+++ src/assignment20/Junit.java
@@ -22,12 +22,31 @@ public class Junit {
 
 	}
 
+	public static void validlast(String str1)
+	{
+		String regex="[A-Z ][a-z]*";
+		Pattern p=Pattern.compile(regex);
+		Matcher m=p.matcher(str1);
+		if(m.find())
+		{
+			System.out.println("yes valid lastname ");
+		}
+		else 
+		{
+			System.out.println("no not a valid lastname ");
+		}
+		
+
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner s=new Scanner(System.in);
 		System.out.println("please enter the firstname ");
 		String firstname=s.nextLine();
 		validfirst(firstname);
+		System.out.println("please enter the lastname ");
+		String lastname=s.nextLine();
+		validlast(lastname);
 	}
 
 }
